964644f86ed0eb7fd5d6d7e1ffb4364a51c7ce2f
Leke <leke.dobruna@gmail.com>
2023-08-09 22:58:18 +0200

    Use `server.hello` function with await

--- client/index.ts
+++ client/index.ts
@@ -14,11 +14,16 @@ const ws = new Socket("ws://localhost:4000", {
 
 const { server } = ws;
 
-ws.events.on("open", () => {
+ws.events.on("open", async () => {
   console.log("Connected to the server");
+
+  const res = await server.hello("John", "Doe");
+  console.log(res); // outputs: [<uuid>] hello John Doe
+
   server.sum(1, 2).then((total) => {
     console.log("Sum of 1 + 2 is", total); // outputs: 3
   });
+
   // @ts-ignore
   server.sum(1, "text").catch(console.error); // outputs: Parameters must be numbers
 });
